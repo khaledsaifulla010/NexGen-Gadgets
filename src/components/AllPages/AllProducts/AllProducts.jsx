219e54cd8af53018c879bdfb6cb161e3db4016fa
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-04 11:06:25 +0600

    loaded all fake data

--- src/components/AllPages/AllProducts/AllProducts.jsx
+++ src/components/AllPages/AllProducts/AllProducts.jsx
@@ -1,11 +1,22 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const AllProducts = () => {
+  const [allProducts, setAllProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("/products.json")
+      .then((res) => res.json())
+      .then((res) => setAllProducts(res));
+  }, []);
+
   return (
     <div>
       <h1 className="text-4xl font-black text-center">
         Explore Cutting-Edge Gadgets
       </h1>
       <div>
-        
+        <h1>Products : {allProducts.length} </h1>
       </div>
     </div>
   );
