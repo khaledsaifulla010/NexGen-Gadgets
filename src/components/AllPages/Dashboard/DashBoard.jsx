22897f05b65b75ca17aafb1472953b60edc06b6b
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-04 20:05:29 +0600

    showing data using sort by price in dashboard

--- src/components/AllPages/Dashboard/DashBoard.jsx
+++ src/components/AllPages/Dashboard/DashBoard.jsx
@@ -3,6 +3,7 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import { MdDeleteForever } from "react-icons/md";
 import { FaDollarSign, FaSortAmountDown } from "react-icons/fa";
+import { useState } from "react";
 const DashBoard = () => {
   const { cartItems } = useCart();
 
@@ -11,6 +12,18 @@ const DashBoard = () => {
     .reduce((sum, price) => sum + price, 0)
     .toFixed(2);
   console.log(totalPrice);
+
+  const [sortCart, setSortCart] = useState(cartItems);
+
+  const handleSortByPrice = (sortType) => {
+    const sortCartByPrice = [...cartItems].sort((a, b) => {
+      if (sortType === "price") {
+        return b.price - a.price;
+      }
+      return 0;
+    });
+    setSortCart(sortCartByPrice);
+  };
 
   return (
     <div>
@@ -56,8 +69,11 @@ const DashBoard = () => {
                     Total Price : <FaDollarSign className="mt-1"></FaDollarSign>
                     {totalPrice}
                   </h1>
-                  <button className="border-2 p-2 text-purple-500 font-black rounded-full text-lg border-purple-500 flex items-center gap-2">
-                    Sort By Price{" "}
+                  <button
+                    onClick={() => handleSortByPrice("price")}
+                    className="border-2 p-2 text-purple-500 font-black rounded-full text-lg border-purple-500 flex items-center gap-2"
+                  >
+                    Sort By Price
                     <FaSortAmountDown className="mt-1"></FaSortAmountDown>
                   </button>
                   <button className="border-2 p-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 font-black rounded-full text-lg text-white">
@@ -71,31 +87,32 @@ const DashBoard = () => {
                 </h1>
               ) : (
                 <div className="mt-12 space-y-10 ">
-                  {cartItems.map((item) => (
-                    <div
-                      key={item.product_id}
-                      className="border-2 w-[1100px] rounded-xl flex items-start gap-6 p-10 justify-between"
-                    >
-                      <img
-                        className="w-[150px] h-[150px] border-2 rounded-lg"
-                        src={item.product_image}
-                      />
-                      <div className="space-y-4">
-                        <h2 className="text-2xl font-black">
-                          {item.product_title}
-                        </h2>
-                        <p className="text-lg font-bold text-slate-500">
-                          {item.description}
-                        </p>
-                        <p className="text-xl font-bold text-slate-600">
-                          Price: ${item.price}
-                        </p>
+                  {cartItems &&
+                    sortCart.map((item) => (
+                      <div
+                        key={item.product_id}
+                        className="border-2 w-[1100px] rounded-xl flex items-start gap-6 p-10 justify-between"
+                      >
+                        <img
+                          className="w-[150px] h-[150px] border-2 rounded-lg"
+                          src={item.product_image}
+                        />
+                        <div className="space-y-4">
+                          <h2 className="text-2xl font-black">
+                            {item.product_title}
+                          </h2>
+                          <p className="text-lg font-bold text-slate-500">
+                            {item.description}
+                          </p>
+                          <p className="text-xl font-bold text-slate-600">
+                            Price: ${item.price}
+                          </p>
+                        </div>
+                        <button>
+                          <MdDeleteForever className="text-3xl mt-12 ml-48"></MdDeleteForever>
+                        </button>
                       </div>
-                      <button>
-                        <MdDeleteForever className="text-3xl mt-12 ml-48"></MdDeleteForever>
-                      </button>
-                    </div>
-                  ))}
+                    ))}
                 </div>
               )}
             </div>
