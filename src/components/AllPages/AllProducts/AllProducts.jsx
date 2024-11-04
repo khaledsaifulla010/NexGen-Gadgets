4a8cca01bd1ebdd7be4c55369bc6f050ffc42ae4
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-04 13:14:22 +0600

    showing all data by category

--- src/components/AllPages/AllProducts/AllProducts.jsx
+++ src/components/AllPages/AllProducts/AllProducts.jsx
@@ -3,17 +3,55 @@ import { useState } from "react";
 import AllProductsCard from "../../AllProductsCard/AllProductsCard";
 
 const AllProducts = () => {
+  const [showProducts, setShowProducts] = useState("all");
   const [allProducts, setAllProducts] = useState([]);
-  const [showProducts, setShowProducts] = useState(false);
+
+  const [allLaptops, setAllLaptops] = useState([]);
+  const [allPhones, setAllPhones] = useState([]);
+  const [allAccessories, setAllAccessories] = useState([]);
+  const [allSmartwatches, setAllSmartwatches] = useState([]);
+  const [allMacbooks, setAllMacbooks] = useState([]);
 
   useEffect(() => {
     fetch("/products.json")
       .then((res) => res.json())
-      .then((res) => setAllProducts(res));
+      .then((res) => {
+        setAllProducts(res);
+        setAllLaptops(res.filter((product) => product.category === "Laptops"));
+        setAllPhones(res.filter((product) => product.category === "Phones"));
+
+        setAllAccessories(
+          res.filter((product) => product.category === "Accessories")
+        );
+        setAllSmartwatches(
+          res.filter((product) => product.category === "Smartwatch")
+        );
+        setAllMacbooks(res.filter((product) => product.category === "MacBook"));
+      });
   }, []);
 
   const handleAllProducts = () => {
-    setShowProducts(!showProducts);
+    setShowProducts("all");
+  };
+
+  const handleAllLaptops = () => {
+    setShowProducts("Laptops");
+  };
+
+  const handleAllPhones = () => {
+    setShowProducts("Phones");
+  };
+
+  const handleAllAccessories = () => {
+    setShowProducts("Accessories");
+  };
+
+  const handleAllSmartwatches = () => {
+    setShowProducts("Smartwatches");
+  };
+
+  const handleAllMacbooks = () => {
+    setShowProducts("Macbooks");
   };
 
   return (
@@ -23,34 +61,62 @@ const AllProducts = () => {
       </h1>
 
       <div className="mt-36 flex gap-12 mb-24">
-        <div className="border-2 w-[200px] h-[400px] ">
+        <div className="border-2 w-[200px] h-[450px] p-2 ">
           <button
             onClick={handleAllProducts}
-            className="border-2 p-2 rounded-xl bg-slate-200 font-black mt-8 ml-10"
+            className=" w-full p-2 rounded-xl bg-slate-100 font-black mt-4 "
           >
             AllProducts
+          </button>
+          <button
+            onClick={handleAllLaptops}
+            className="w-full  p-2 rounded-xl bg-slate-100 font-black mt-8 "
+          >
+            Laptops
+          </button>
+          <button
+            onClick={handleAllPhones}
+            className="w-full  p-2 rounded-xl bg-slate-100 font-black mt-8 "
+          >
+            Phones
+          </button>
+          <button
+            onClick={handleAllAccessories}
+            className="w-full  p-2 rounded-xl bg-slate-100 font-black mt-8 "
+          >
+            Accessories
+          </button>
+          <button
+            onClick={handleAllSmartwatches}
+            className="w-full  p-2 rounded-xl bg-slate-100 font-black mt-8 "
+          >
+            Smartwatch
+          </button>
+          <button
+            onClick={handleAllMacbooks}
+            className="w-full  p-2 rounded-xl bg-slate-100 font-black mt-8 "
+          >
+            Macbooks
           </button>
         </div>
 
         <div className=" w-[1100px] h-full">
-          <div className=" grid grid-cols-3 gap-8">
-            {allProducts.map((product) => (
-              <AllProductsCard
-                product={product}
-                key={product.product_id}
-              ></AllProductsCard>
+          <div className="grid grid-cols-3 gap-8">
+            {(showProducts === "all"
+              ? allProducts
+              : showProducts === "Laptops"
+              ? allLaptops
+              : showProducts === "Phones"
+              ? allPhones
+              : showProducts === "Accessories"
+              ? allAccessories
+              : showProducts === "Smartwatches"
+              ? allSmartwatches
+              : allMacbooks
+            ).map((product) => (
+              <AllProductsCard product={product} key={product.product_id} />
             ))}
           </div>
-          {showProducts && (
-            <div className=" grid grid-cols-3 gap-8">
-              {allProducts.map((product) => (
-                <AllProductsCard
-                  product={product}
-                  key={product.product_id}
-                ></AllProductsCard>
-              ))}
-            </div>
-          )}
         </div>
       </div>
     </div>
