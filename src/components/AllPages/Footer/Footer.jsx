c523766b8e5ae6511ae3266e9cae329829c47510
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-05 18:11:28 +0600

    added footer

--- src/components/AllPages/Footer/Footer.jsx
+++ src/components/AllPages/Footer/Footer.jsx
@@ -1,11 +1,39 @@
-
-
 const Footer = () => {
-    return (
-      <div>
-        <h1>This is Footer</h1>
+  return (
+    <div>
+      <div className="text-center">
+        <h1 className="font-bold text-3xl">NexGen Gadgets</h1>
+        <p className="font-semibold text-lg mt-1">
+          Leading the way in cutting-edge technology and innovation.
+        </p>
       </div>
-    );
+      <div className="divider px-4"></div>
+      <div>
+        <footer className="footer ml-64 w-[1000px]  p-10">
+          <nav>
+            <h6 className="font-bold text-xl">Services</h6>
+            <a className="link link-hover">Branding</a>
+            <a className="link link-hover">Design</a>
+            <a className="link link-hover">Marketing</a>
+            <a className="link link-hover">Advertisement</a>
+          </nav>
+          <nav>
+            <h6 className="font-bold text-xl">Company</h6>
+            <a className="link link-hover">About us</a>
+            <a className="link link-hover">Contact</a>
+            <a className="link link-hover">Jobs</a>
+            <a className="link link-hover">Press kit</a>
+          </nav>
+          <nav>
+            <h6 className="font-bold text-xl">Legal</h6>
+            <a className="link link-hover">Terms of use</a>
+            <a className="link link-hover">Privacy policy</a>
+            <a className="link link-hover">Cookie policy</a>
+          </nav>
+        </footer>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../AllPages/NavBar/NavBar";
+import Footer from '../../components/AllPages/Footer/Footer'
 
 const MainLayout = () => {
   return (
     <div className="max-w-[1300px] mx-auto mt-6">
       <NavBar></NavBar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
